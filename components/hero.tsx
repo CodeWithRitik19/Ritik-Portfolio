cb665d8bea8d9b679e60877bc97ef6a065d2907f
Ritik Kumar <145904770+CodeWithRitik19@users.noreply.github.com>
2025-12-13 03:35:03 +0530

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
                 transition={{ duration: 0.8 }}
             >
             <span className="block bg-gradient-to-r from-[#3EADCF] via-[#57D9FF] to-[#0ea5e9] bg-clip-text text-transparent">
-              Ritik Kumar Love Sneha
+              Ritik Kumar 
             </span>
             </motion.h1>
 
